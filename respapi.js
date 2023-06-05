9e6be6ef89ed0bebc2b908e0a2296a3c44c3b8d8
oka.ats@gmail.com <oka.ats@gmail.com>
2023-06-05 20:02:09 +0900

    implement respapi() a new mechanism to invoke a method on an object

--- respapi.js
+++ respapi.js
@@ -2,16 +2,16 @@
 const { schema             } = require( 'vanilla-schema-validator' );
 const { typesafe_function, get_typesafe_tags } = require( 'runtime-typesafety' );
 
+
 /**
  * status : 'found'
  *          'forbidden'
  *          'not_found'
- *
  */
 const resolve_callapi_method_path = typesafe_function(
   function resolve_callapi_method_path( callapi_target, callapi_method_path, required_typesafe_tag ) {
     const accumlator = {
-      status       : 'found',
+      status               : 'found',
       value                : callapi_target,
       tags                 : [],
       actual_method_path   : [], // was valid_prop_name_list (Fri, 02 Jun 2023 13:12:29 +0900)
@@ -24,48 +24,43 @@ const resolve_callapi_method_path = typesafe_function(
       return r;
     };
 
-    try{
-      const result = callapi_method_path.reduce((accumlator,prop_name)=>{
-        if ( prop_name === undefined || prop_name === null ) {
-          throw new ReferenceError( `internal error; prop_name value should not be undefined or null ${prop_name}` );
-        } else if ( accumlator.status !== 'found' ) {
-          // CONDITION_ABOVE
-          return accumlator;
-        } else if ( prop_name in accumlator.value ) {
-          const next_value = accumlator.value[prop_name];
-          const tags       = next_value ? ( get_typesafe_tags( next_value ) ?? [] ) : [];
+    const result = callapi_method_path.reduce((accumlator,prop_name)=>{
+      if ( prop_name === undefined || prop_name === null ) {
+        throw new ReferenceError( `internal error; prop_name value should not be undefined or null ${prop_name}` );
+      } else if ( accumlator.status !== 'found' ) {
+        // CONDITION_ABOVE
+        return accumlator;
+      } else if ( prop_name in accumlator.value ) {
+        const next_value = accumlator.value[prop_name];
+        const tags       = next_value ? ( get_typesafe_tags( next_value ) ?? [] ) : [];
 
-          if ( tags.includes( required_typesafe_tag ) ) {
-            return {
-              status             : 'found',
-              value              : next_value,
-              tags               : tags,
-              actual_method_path : [ ...accumlator.actual_method_path  , prop_name ],
-            };
-          } else {
-            return {
-              status             : 'forbidden', // see the CONDITION_ABOVE
-              value              : null,
-              tags               : tags,
-              actual_method_path : [ ...accumlator.actual_method_path  , prop_name ],
-            };
-          }
+        if ( tags.includes( required_typesafe_tag ) ) {
+          return {
+            status             : 'found',
+            value              : next_value,
+            tags               : tags,
+            actual_method_path : [ ...accumlator.actual_method_path  , prop_name ],
+          };
         } else {
           return {
-            status         : 'not_found', // see the CONDITION_ABOVE
-            value          : null,
-            tags           : [],
-            actual_method_path   : accumlator.actual_method_path  ,
+            status             : 'forbidden', // see the CONDITION_ABOVE
+            value              : null,
+            tags               : tags,
+            actual_method_path : [ ...accumlator.actual_method_path  , prop_name ],
           };
         }
-      }, accumlator );
+      } else {
+        return {
+          status         : 'not_found', // see the CONDITION_ABOVE
+          value          : null,
+          tags           : [],
+          actual_method_path   : accumlator.actual_method_path  ,
+        };
+      }
+    }, accumlator );
 
-      return finallization(result);
+    return finallization(result);
 
-    } catch (err) {
-      finallization( err.callapi_result );
-      throw err;
-    }
   },{
     typesafe_input : schema.compile`
       array(
@@ -97,6 +92,67 @@ const resolve_callapi_method_path = typesafe_function(
     `,
   }
 );
-
 module.exports.resolve_callapi_method_path = resolve_callapi_method_path;
+
+
+const respapi = typesafe_function(
+  async function respapi( callapi_target, callapi_method_path, required_typesafe_tag, on_execution ) {
+    const resolved_callapi_method = resolve_callapi_method_path( callapi_target, callapi_method_path, required_typesafe_tag );
+
+    if ( resolved_callapi_method.status === 'found' ) {
+      try {
+        const value = await on_execution( resolved_callapi_method.value );
+        return {
+          ...resolved_callapi_method,
+          status : 'succeeded',
+          value  : value,
+        };
+      } catch (err) {
+        return {
+          ...resolved_callapi_method,
+          status : 'error',
+          value  : err,
+        };
+      }
+    } else {
+      return resolved_callapi_method;
+    }
+
+  }, {
+    typesafe_input : schema.compile`
+      array(
+        callapi_target        : object(),
+        callapi_method_path   : array_of( string() ),
+        required_typesafe_tag : string(),
+        on_execution          : function(),
+      ),
+    `,
+    typesafe_output : schema.compile`
+      object(
+        status : and(
+          string(),
+          or(
+            equals( <<'succeeded'>> ),
+            equals( <<'error'>>     ),
+            equals( <<'not_found'>> ),
+            equals( <<'forbidden'>> ),
+          ),
+        ),
+        value  : any(),
+        tags                       : array_of( string() ),
+        actual_method_path         : array_of( string() ),
+        callapi_method_path        : array_of( string() ),
+        actual_method_path_string  : string(),
+        callapi_method_path_string : string(),
+      )
+    `,
+  }
+);
+
+module.exports.respapi = respapi;
+
+
+
+
+
 
