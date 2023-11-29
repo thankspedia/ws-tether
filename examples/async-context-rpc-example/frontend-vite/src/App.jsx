bacf36dd708355e03bc738ae50d1f829cca35c35
oka.ats@gmail.com <oka.ats@gmail.com>
2023-11-29 12:07:27 +0900

    add 'import' to Vite and notice everything must be ESM

--- examples/async-context-rpc-example/frontend-vite/src/App.jsx
+++ examples/async-context-rpc-example/frontend-vite/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { createContext as __createContext } from  'asynchronous-context-rpc/ws-frontend-callapi-context-factory' ;
+
 function App() {
   const [count, setCount] = useState(0)
 
